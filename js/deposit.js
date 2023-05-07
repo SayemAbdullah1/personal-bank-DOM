eb2a58e97e76759a8a02f6d5dfc4fb528330b052
SayemAbdullah1 <dxsayem0@gmail.com>
2023-05-07 09:36:36 +0600

    add total deposit

--- js/deposit.js
+++ js/deposit.js
@@ -9,5 +9,14 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
 
     const currentDepositTotal = preDeposit + newDeposit;
     depositTotalElement.innerText = currentDepositTotal;
+
+    const preDepositTotal = document.getElementById('total-balance')
+    const preTotal = preDepositTotal.innerText
+    const preTotalElement = parseFloat(preTotal)
+
+    const totalDeposit = preTotalElement + currentDepositTotal;
+    preDepositTotal.innerText = totalDeposit;
+    // console.log(preTotalElement)
+
     deposit.value = '';
 })
